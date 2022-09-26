a128814e1465d7269e993b7cbf727580f17200ae
jonathans199c <jonathasa@city-furniture.com>
2022-09-26 12:30:14 -0400

    added firebase hosting

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -2,12 +2,9 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
+import bclogo from '../assets/bc-logo-white.png'
 
 import { AppSidebarNav } from './AppSidebarNav'
-
-import { logoNegative } from 'src/assets/brand/logo-negative'
-import { sygnet } from 'src/assets/brand/sygnet'
 
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
@@ -30,8 +27,7 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarBrand className="d-none d-md-flex" to="/">
-        <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
-        <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
+        <img src={bclogo} alt="" height="35" />
       </CSidebarBrand>
       <CSidebarNav>
         <SimpleBar>
