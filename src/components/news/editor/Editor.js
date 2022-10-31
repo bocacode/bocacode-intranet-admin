2a705aaaf9badc984a18896680e7f53025fd84dd
ralphZero <placideralph@gmail.com>
2022-10-31 18:39:08 -0400

    building editor quill

---
+++ src/components/news/editor/Editor.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+
+const Editor = () => {
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['link', 'image', 'video'],
+    ],
+  }
+
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+    'video',
+    'code-block',
+  ]
+
+  return (
+    <div className="editor-parent container-fluid p-0 flex-grow-1 ">
+      <ReactQuill theme="snow" modules={modules} formats={formats} />
+    </div>
+  )
+}
+
+export default Editor
--- src/views/news/write-article/WriteArticle.js
+++ src/views/news/write-article/WriteArticle.js
@@ -1,76 +1,91 @@
 import React, { useState, useEffect } from 'react'
-import { CCol, CRow, CCard, CButton, CCardHeader, CForm } from '@coreui/react'
+import {
+  CCol,
+  CRow,
+  CCard,
+  CButton,
+  CCardHeader,
+  CForm,
+  CCardBody,
+  CFormInput,
+} from '@coreui/react'
 import {
   FirestoreGetOneNews,
   FirestorePostNews,
   FirestoreUpdateOneNews,
 } from 'src/utils/firebaseUtils'
 import { useSearchParams } from 'react-router-dom'
+import Editor from 'src/components/news/editor/Editor'
 
 const WriteArticle = () => {
   const [isUpdating, setIsUpdating] = useState(false)
   const [news, setNews] = useState()
   const [searchParams, setSearchParams] = useSearchParams()
 
-  const handleSubmit = (e) => {
-    console.log(news)
-    e.preventDefault()
-    if (!news.title) {
-      return false
-    }
+  useEffect(() => {
+    const body = document.querySelector('.body')
+    const footer = document.querySelector('.footer')
+    const container = document.querySelector('.editor-page')
+    const editor = document.querySelector('.ql-container')
+    const editorParent = document.querySelector('.editor-parent')
+    const toolbar = document.querySelector('.ql-toolbar')
+    container.style.minHeight = body.clientHeight + 'px'
+    editor.style.minHeight = editorParent.clientHeight - toolbar.clientHeight + 'px'
+    window.addEventListener('resize', () => {
+      container.style.minHeight = body.clientHeight + 'px'
+      editor.style.minHeight = editorParent.clientHeight - toolbar.clientHeight + 'px'
+    })
+  }, [])
 
-    if (!isUpdating) {
-      FirestorePostNews(news).then((docId) => {
-        console.log('docId', docId)
-        setNews(null)
-      })
-    } else {
-      FirestoreUpdateOneNews(news).then(() => {
-        setSearchParams({ id: '' })
-      })
-    }
-  }
+  // const handleSubmit = (e) => {
+  //   console.log(news)
+  //   e.preventDefault()
+  //   if (!news.title) {
+  //     return false
+  //   }
+
+  //   if (!isUpdating) {
+  //     FirestorePostNews(news).then((docId) => {
+  //       console.log('docId', docId)
+  //       setNews(null)
+  //     })
+  //   } else {
+  //     FirestoreUpdateOneNews(news).then(() => {
+  //       setSearchParams({ id: '' })
+  //     })
+  //   }
+  // }
 
   const handleTitleChange = (title) => {
     setNews({ ...news, title: title })
   }
 
-  const handleContentChange = (content) => {
-    const preview = content.blocks.filter((block) => {
-      return block.type === 'unstyled' && block.text.length !== 0
-    })
-
-    const thumbnailPreview = (preview[0]?.text + ' ' + preview[1]?.text).substring(0, 250) + '...'
-
-    setNews({ ...news, body: thumbnailPreview, data: content })
-  }
-
-  useEffect(() => {
-    const id = searchParams.get('id')
-    if (id) {
-      setNews(null)
-      FirestoreGetOneNews(id)
-        .then((value) => {
-          if (value) {
-            setIsUpdating(true)
-            setNews(value)
-          } else {
-            setIsUpdating(false)
-          }
-        })
-        .catch((err) => {
-          console.error(err)
-          setIsUpdating(false)
-        })
-    } else {
-      setIsUpdating(false)
-    }
-  }, [searchParams])
+  // useEffect(() => {
+  //   const id = searchParams.get('id')
+  //   if (id) {
+  //     setNews(null)
+  //     FirestoreGetOneNews(id)
+  //       .then((value) => {
+  //         if (value) {
+  //           setIsUpdating(true)
+  //           setNews(value)
+  //         } else {
+  //           setIsUpdating(false)
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.error(err)
+  //         setIsUpdating(false)
+  //       })
+  //   } else {
+  //     setIsUpdating(false)
+  //   }
+  // }, [searchParams])
 
   return (
-    <CRow>
+    <CRow className="editor-page">
       <CCol>
-        <CForm onSubmit={handleSubmit} className="d-flex flex-column gap-3 h-100">
+        <CForm className="d-flex flex-column gap-3 h-100">
           <CCard>
             <CCardHeader className="d-flex justify-content-between align-items-center">
               <strong>Write</strong>
@@ -79,26 +94,19 @@ const WriteArticle = () => {
               </CButton>
             </CCardHeader>
           </CCard>
-          {/* <CCard className="flex-grow-1">
-            <CCardBody>
+          <CCard className="flex-grow-1">
+            <CCardBody className="d-flex flex-column">
               <CFormInput
                 value={news?.title}
                 onChange={(e) => setNews({ ...news, title: e.target.value })}
-                className="mb-3"
+                className="mb-3 rounded-0 fs-1 fw-bold"
                 required
                 placeholder="Title here"
                 type="text"
               />
-              <CFormTextarea
-                required
-                id="exampleFormControlTextarea1"
-                rows="5"
-                placeholder="Write here"
-                value={news?.body}
-                onChange={(e) => setNews({ ...news, body: e.target.value })}
-              ></CFormTextarea>
+              <Editor />
             </CCardBody>
-          </CCard> */}
+          </CCard>
         </CForm>
       </CCol>
     </CRow>
